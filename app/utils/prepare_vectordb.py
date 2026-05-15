73d12bddd8c36c9ccea5cf05b20b3039c7c2615a
vinay23is <55172995+vinay23is@users.noreply.github.com>
2026-05-15 12:21:55 -0500

    Fix: update text splitter import for LangChain 0.2+

--- app/utils/prepare_vectordb.py
+++ app/utils/prepare_vectordb.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from dotenv import load_dotenv
